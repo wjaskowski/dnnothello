dd76d969dfe45290a56b94cc2c5d3a6b2f8cba59
Wojciech Jaśkowski <wojciech.jaskowski@gmail.com>
2015-09-25 14:26:58 +0200

    Reading learning data from Othello game databases

---
+++ othello.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+WALL = -1
+WHITE = 0
+EMPTY = 1
+BLACK = 2
+
+SIZE = 8
+WIDTH = SIZE + 2
+
+DIRS = ((-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1))
+
+def new_board():
+    board = np.ones((WIDTH, WIDTH), np.int8) * EMPTY
+    M = WIDTH // 2
+    board[M-1, M-1] = WHITE
+    board[M-1, M] = BLACK
+    board[M, M] = WHITE
+    board[M, M-1] = BLACK
+
+    board[[0, WIDTH-1], :] = WALL
+    board[:, [0, WIDTH-1]] = WALL
+    return board
+
+
+def make_move(board, move, player):
+    move_row, move_col = move
+    assert 1 <= move_row <= SIZE, "row = " + str(move_row)
+    assert 1 <= move_col <= SIZE, "col = " + str(move_col)
+    assert player in (WHITE, BLACK)
+
+    for dirx, diry in DIRS:
+        x = move_col + dirx
+        y = move_row + diry
+
+        while board[y, x] == opponent(player):
+            x += dirx
+            y += diry
+
+        if board[y, x] == player:
+            x -= dirx
+            y -= diry
+            while board[y, x] == opponent(player):
+                board[y, x] = player
+                x -= dirx
+                y -= diry
+    board[move_row, move_col] = player
+
+
+def is_valid_move(board, move, player):
+    move_row, move_col = move
+    assert 1 <= move_row <= SIZE
+    assert 1 <= move_col <= SIZE
+    assert player in (WHITE, BLACK)
+
+    if board[move_row, move_col] != EMPTY:
+        return False
+
+    for dirx, diry in DIRS:
+        x = move_col + dirx
+        y = move_row + diry
+
+        while board[y, x] == opponent(player):
+            x += dirx
+            y += diry
+
+        if board[y, x] == player and board[y - diry, x - dirx] == opponent(player):
+            return True
+    return False
+
+
+def valid_moves(board, player):
+    for row in range(1,SIZE+1):
+        for col in range(1,SIZE+1):
+            if is_valid_move(board, (row, col), player):
+                yield row, col
+
+def has_valid_moves(board, player):
+    return len(list(valid_moves(board, player))) > 0
+
+
+def opponent(player):
+    assert player in (WHITE, BLACK)
+    return WHITE if player == BLACK else BLACK
+
+
+def get_score(board):
+    b = (board[1:-1,1:-1] == BLACK).sum()
+    w = (board[1:-1,1:-1] == WHITE).sum()
+    return b,w
+
+
+def get_true_score(board):
+    b, w = get_score(board)
+    e = SIZE*SIZE - b - w
+    if b > w:
+        return b+e, w
+    elif b < w:
+        return b, w+e
+    else:
+        return b+e//2, w+e//2
---
+++ othello_data.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+import struct
+import glob
+import os.path
+import othello
+import numpy as np
+import os,sys
+import pickle
+import gzip
+
+
+def read_wthor(file_name):
+    with open(file_name, mode='rb') as f:
+        header = struct.unpack("<BBBBIHHBBBB", f.read(16))
+        games = header[4]
+        year = header[6]
+        board_size = header[7]
+        assert board_size == 0 or board_size == 8
+
+        for i in range(games):
+            game_info = struct.unpack("<HHHBB", f.read(8))
+            true_score=game_info[3] 
+            moves = struct.unpack('<' + 'B'*60, f.read(60))
+            yield true_score, moves
+
+
+def read_all(path):
+    for wtb in glob.glob(os.path.join(path, '*.wtb')):
+        yield from read_wthor(wtb)
+
+def generate_learning_data(path):
+    games = read_all(path)
+    for i, (true_score, moves) in enumerate(games):
+        if i % 1000 == 0: print(i)
+        yield from moves2data(true_score, moves)
+
+def moves2data(true_score, moves):
+    board = othello.new_board()
+    player = othello.BLACK
+    moves = [m for m in moves if m != 0] # remove (trailing) zeros
+    for move in moves:
+        row, col = move // othello.WIDTH, move % othello.WIDTH
+        if not othello.is_valid_move(board, (row, col), player):
+            player = othello.opponent(player)
+            assert othello.is_valid_move(board, (row, col), player)
+        yield np.copy(board[1:-1,1:-1]), player, move
+        othello.make_move(board, (row, col), player)
+        player = othello.opponent(player)
+    if othello.get_true_score(board)[0] != true_score:
+        print("WARNING: {} != {}".format(othello.get_true_score(board), true_score))
+
+def get_learning_data():
+    return pickle.load(gzip.open('data.dump', 'rb'))
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2 and sys.argv[1] == 'generate':
+        data = list(generate_learning_data('wthor_data'))
+        pickle.dump(data, gzip.open('data.dump', 'wb'))
+
+    data = get_learning_data()
+    print("read:", len(data), "positions")
+    print("example:", data[0])
